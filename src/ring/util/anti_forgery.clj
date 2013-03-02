3251a2fc933a18dc63d5842856b1f631509b75d2
Ryan McGowan <ryan@ryanmcg.com>
2013-03-02 15:09:12 -0500

    Add util/anti-forgery-metas to easily generate meta tags.

--- src/ring/util/anti_forgery.clj
+++ src/ring/util/anti_forgery.clj
@@ -1,5 +1,5 @@
 (ns ring.util.anti-forgery
-  (:use [hiccup core form]
+  (:use [hiccup core form def]
         ring.middleware.anti-forgery))
 
 (defn anti-forgery-field
@@ -8,3 +8,10 @@
   middleware."
   []
   (html (hidden-field "__anti-forgery-token" *anti-forgery-token*)))
+
+(defhtml anti-forgery-metas
+  "Create meta tags for specifying an anti-forgery-token values to be put in
+  request headers."
+  []
+  [:meta {:name "csrf_header" :content "X-Anti-Forgery-Token"}]
+  [:meta {:name "csrf_token" :content *anti-forgery-token*}])
