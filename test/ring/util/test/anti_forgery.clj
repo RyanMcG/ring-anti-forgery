50b60f06d9972e5c50ca8986602f6cec796c829f
Ryan McGowan <ryan@ryanmcg.com>
2013-03-02 15:09:26 -0500

    Test anti-forgery-metas.

--- test/ring/util/test/anti_forgery.clj
+++ test/ring/util/test/anti_forgery.clj
@@ -8,3 +8,9 @@
     (is (= (anti-forgery-field)
            (str "<input id=\"__anti-forgery-token\" name=\"__anti-forgery-token\""
                 " type=\"hidden\" value=\"abc\" />")))))
+
+(deftest anti-forgery-metas-test
+  (binding [*anti-forgery-token* "abc"]
+    (is (= (anti-forgery-metas)
+           (str "<meta content=\"X-Anti-Forgery-Token\" name=\"csrf_header\" />"
+                "<meta content=\"abc\" name=\"csrf_token\" />")))))
